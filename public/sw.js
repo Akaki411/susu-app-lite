05fc30f0d7adf1bcf9b3acd644c4effb070c0eb1
Akaki411 <akovalysko123@gmail.com>
2026-09-08 03:43:57 +0500

    ci: change app version

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
   При обновлении версии CACHE старые кеши удаляются.
 */
 
-const CACHE = 'susu-lite-v1.0'
+const CACHE = 'susu-lite-v1.1'
 const CORE = ['/schedule', '/rating', '/services', '/manifest.webmanifest', '/logo.webp', '/icon.png']
 
 self.addEventListener('install', (event) => {
